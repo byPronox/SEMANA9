dc182bf14fb28665c00e4c36086fe825af9081ff
Laboratorio <laboratorio@udla-ec.int>
2023-05-22 16:14:53 -0500

    Final con comentario

--- MATRIZ TRANSPUESTA.c
+++ MATRIZ TRANSPUESTA.c
@@ -3,16 +3,22 @@
 
 int main()
 {
+    //Definicion de variables
     int c;
     int f;
 
+    //Solicitud al usuario de la matriz
     printf("Ingrese el numero de columnas de la matriz: ");
     scanf("%d", &c);
 
     printf("Ingrese el numero de filas de la matriz: ");
     scanf("%d", &f);
 
+    //Definicion de la matriz
     int num[f][c];
+
+    // Impresion y los valores aleatorios de matriz original
+    printf("Su matriz original es:\n");
     
 
     for (int i = 0; i < f; i++)
@@ -25,6 +31,10 @@ int main()
         printf("\n");
     }
     printf("\n");
+
+    //Impresion de la matriz transpuesta
+
+    printf("Su transpuesta es:\n");
     
     for (int i = 0; i < f; i++)
     {
